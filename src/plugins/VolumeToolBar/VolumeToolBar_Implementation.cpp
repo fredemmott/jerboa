dbdaf5ae7973d1b9e801b1b5d8ceb7995051b120
Fred Emmott <mail@fredemmott.co.uk>
2009-06-05 13:21:39 +0100

    working volume toolbar

--- src/plugins/VolumeToolBar/VolumeToolBar_Implementation.cpp
+++ src/plugins/VolumeToolBar/VolumeToolBar_Implementation.cpp
@@ -18,9 +18,34 @@ VolumeToolBar::Implementation::Implementation(
 , m_slider(new QSlider(Qt::Horizontal, this))
 {
 	m_slider->setToolTip(tr("Volume"));
+	m_slider->setRange(0, 100);
+	volumeChanged(player->volume());
+
 	QLabel* label = new QLabel(this);
 	QIcon icon = style()->standardIcon(QStyle::SP_MediaVolume);
 	label->setPixmap(icon.pixmap(icon.availableSizes().first()));
 	addWidget(label);
 	addWidget(m_slider);
+
+	connect(
+		m_slider,
+		SIGNAL(valueChanged(int)),
+		SLOT(changeVolume(int))
+	);
+
+	connect(
+		m_player,
+		SIGNAL(volumeChanged(qreal)),
+		SLOT(volumeChanged(qreal))
+	);
+}
+
+void VolumeToolBar::Implementation::changeVolume(int value)
+{
+	m_player->setVolume(value / 100.0);
+}
+
+void VolumeToolBar::Implementation::volumeChanged(qreal value)
+{
+	m_slider->setValue(qRound(value * 100));
 }
--- src/plugins/VolumeToolBar/VolumeToolBar_Implementation.h
+++ src/plugins/VolumeToolBar/VolumeToolBar_Implementation.h
@@ -13,6 +13,9 @@ class VolumeToolBar::Implementation : public QToolBar
 	Q_OBJECT;
 	public:
 		Implementation(Jerboa::PlayerInterface* player, QWidget* parent);
+	private slots:
+		void changeVolume(int value);
+		void volumeChanged(qreal value);
 	private:
 		Jerboa::PlayerInterface* m_player;
 		QSlider* m_slider;
