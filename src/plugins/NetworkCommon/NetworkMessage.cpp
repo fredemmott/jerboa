20eee004961781fb6c75d975cdd63fcf5816ea6d
Fred Emmott <mail@fredemmott.co.uk>
2010-02-21 12:22:55 +0000

    add some more message handling code

--- src/plugins/NetworkCommon/NetworkMessage.cpp
+++ src/plugins/NetworkCommon/NetworkMessage.cpp
@@ -1,5 +1,7 @@
 #include "NetworkMessage.h"
 
+#include <QDebug>
+
 NetworkMessage::NetworkMessage(const QByteArray& command, const QByteArray& payload, MessageType messageType)
 {
 	m_command = command;
@@ -7,14 +9,77 @@ NetworkMessage::NetworkMessage(const QByteArray& command, const QByteArray& payl
 	m_messageType = messageType;
 }
 
+NetworkMessage::NetworkMessage(const QByteArray& command, const QList<QByteArray>& payload, MessageType messageType)
+{
+	m_command = command;
+	m_messageType = messageType;
+
+	m_payloadList = payload;
+
+	Q_FOREACH(const QByteArray& item, payload)
+	{
+		m_payload.append(QByteArray::number(item.length()));
+		m_payload.append(':');
+		m_payload.append(item);
+		m_payload.append(',');
+	}
+}
+
+NetworkMessage::NetworkMessage(const QByteArray& serialized)
+{
+	const int endOfCommandLength = serialized.indexOf(':');
+	const int commandLength = serialized.left(endOfCommandLength).toInt();
+
+	const int startOfCommand = endOfCommandLength + 1;
+	m_command = serialized.mid(startOfCommand, commandLength);
+	const int endOfCommand = startOfCommand + commandLength;
+
+	const int startOfPayloadLength = endOfCommand + 1;
+	const int endOfPayloadLength = serialized.indexOf(':', startOfPayloadLength);
+	const int payloadLength = serialized.mid(startOfPayloadLength, endOfPayloadLength - startOfPayloadLength).toInt();
+
+	const int startOfPayload = endOfPayloadLength + 1;
+	m_payload = serialized.mid(startOfPayload, payloadLength);
+
+	m_messageType = UnsignedMessage;
+}
+
+QByteArray NetworkMessage::command() const
+{
+	return m_command;
+}
+
+QList<QByteArray> NetworkMessage::payloadList() const
+{
+	if(m_payloadList.isEmpty())
+	{
+		// "size:data,"
+		int left = 0;
+		while(left < m_payload.size())
+		{
+			const QByteArray chunk = m_payload.mid(left);
+			const int separatorPosition = chunk.indexOf(':');
+			const QByteArray lengthChunk = chunk.left(separatorPosition);
+			const int length = lengthChunk.toInt();
+			qDebug() << Q_FUNC_INFO << "Length:" << lengthChunk << length;
+			Q_ASSERT(chunk.size() >= length + lengthChunk.size() + 2);
+			const QByteArray data = chunk.mid(separatorPosition + 1, length);
+			Q_ASSERT(data.length() == length);
+			left += lengthChunk.size() + 1 + length + 1;
+			m_payloadList.append(data);
+		}
+	}
+	return m_payloadList;
+}
+
 void NetworkMessage::updateSerialization() const
 {
 	// netstrings-influenced
-	// CommandLength,Command,PayloadLength,Payload,{U|S,timestamp-length,timestamp,message-uuid,signature};
+	// CommandLength:Command,PayloadLength:Payload,{U|S,timestamp-length:timestamp,message-uuid,signature};
 	//
 	// Examples:
-	// 3,foo,0,,U;
-	// 3,bar,3,baz,S,10,1266356591,{fc8d8c6c-2ede-4bb8-8685-2cc47cdd8b2b},0000000000000000000000000000000000000000
+	// 3:foo,0:,U;
+	// 3:bar,3:baz,S,10:1266356591,{fc8d8c6c-2ede-4bb8-8685-2cc47cdd8b2b},0000000000000000000000000000000000000000
 	//
 	// Where:
 	// - timestamp is the number of seconds since 1970-01-01T00:00:00 UTC
@@ -24,12 +89,12 @@ void NetworkMessage::updateSerialization() const
 	m_serialized.clear();
 	Q_ASSERT(m_messageType == UnsignedMessage);
 
-	out.append(QString::number(m_command.length()).toLatin1());
-	out.append(',');
+	out.append(QByteArray::number(m_command.length()));
+	out.append(':');
 	out.append(m_command);
 	out.append(',');
-	out.append(QString::number(m_payload.length()).toLatin1());
-	out.append(',');
+	out.append(QByteArray::number(m_payload.length()));
+	out.append(':');
 	out.append(m_payload);
 	out.append(",U;");
 }
--- src/plugins/NetworkCommon/NetworkMessage.h
+++ src/plugins/NetworkCommon/NetworkMessage.h
@@ -2,6 +2,7 @@
 
 #include <QByteArray>
 #include <QIODevice>
+#include <QList>
 
 class NetworkMessage
 {
@@ -12,13 +13,21 @@ class NetworkMessage
 			SignedMessage
 		};
 		NetworkMessage(const QByteArray& command, const QByteArray& payload, MessageType messageType);
+		NetworkMessage(const QByteArray& command, const QList<QByteArray>& payload, MessageType messageType);
+		NetworkMessage(const QByteArray& serialized);
+
+		QByteArray command() const;
+		QByteArray payload() const;
+		QList<QByteArray> payloadList() const;
+
 		QByteArray toByteArray() const;
 	private:
 		void updateSerialization() const; // cache
 
 		QByteArray m_command;
 		QByteArray m_payload;
+		mutable QList<QByteArray> m_payloadList; // cache
 		MessageType m_messageType;
 
-		mutable QByteArray m_serialized; // used as a cache
+		mutable QByteArray m_serialized; // cache
 };
